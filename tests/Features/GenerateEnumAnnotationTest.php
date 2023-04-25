30cb22216b8fde47de35447df176c05620993ee2
陆云峰 <v-luyunfeng@xiaomi.com>
2023-04-25 21:12:35 +0800

    phpunit

--- tests/Features/GenerateEnumAnnotationTest.php
+++ tests/Features/GenerateEnumAnnotationTest.php
@@ -2,29 +2,19 @@
 
 use BiiiiiigMonster\LaravelEnum\Tests\Enums\Week;
 use Illuminate\Filesystem\Filesystem;
-use Laminas\Code\Generator\DocBlock\Tag\MethodTag;
-use Laminas\Code\Generator\DocBlock\Tag\TagInterface;
-use Laminas\Code\Generator\DocBlockGenerator;
-use Laminas\Code\Reflection\DocBlockReflection;
+use Mockery\MockInterface;
 
 it('can generate method docComment for enums', function () {
     $filePutContents = '';
-    $mockFilesystem = Mockery::mock(Filesystem::class);
-    $mockFilesystem
-        ->shouldReceive('get')
-        ->andReturnUsing(function ($file) {
-            return file_get_contents($file);
-        });
+    $this->partialMock(Filesystem::class, function (MockInterface $mock) use (&$filePutContents) {
+        $mock->shouldReceive('put')
+            ->andReturnUsing(function ($path, $contents) use (&$filePutContents) {
+                $filePutContents .= $contents;
 
-    $mockFilesystem
-        ->shouldReceive('put')
-        ->with(Mockery::any(), Mockery::any())
-        ->andReturnUsing(function ($path, $contents) use (&$filePutContents) {
-            $filePutContents .= $contents;
+                return file_put_contents($path, $contents);
+            });
+    });
 
-            return strlen($contents);
-        });
-    $this->instance(Filesystem::class, $mockFilesystem);
     $this->artisan('enum:annotate', ['enum' => Week::class]);
 
     expect($filePutContents)
