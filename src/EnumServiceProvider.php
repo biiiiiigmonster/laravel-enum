5283a89666730a4bfcc703a6d7ce61e46bff3fbb
biiiiiigmonster <biiiiiigmonster@users.noreply.github.com>
2023-06-27 14:07:32 +0000

    Fix styling

--- src/EnumServiceProvider.php
+++ src/EnumServiceProvider.php
@@ -28,23 +28,23 @@ class EnumServiceProvider extends ServiceProvider
 
     private function bootValidators(): void
     {
-        echo '看看enum字符串：' . trans(static::LANG_NAMESPACE . '::validation.enum');
+        echo '看看enum字符串：'.trans(static::LANG_NAMESPACE.'::validation.enum');
         Validator::extend('enum', function ($attribute, $value, $parameters, $validator) {
             return (new Enum(...$parameters))->passes($attribute, $value);
-        }, trans(static::LANG_NAMESPACE . '::validation.enum'));
+        }, trans(static::LANG_NAMESPACE.'::validation.enum'));
 
-        echo '看看enum_meta字符串：' . trans(static::LANG_NAMESPACE . '::validation.enum_meta');
+        echo '看看enum_meta字符串：'.trans(static::LANG_NAMESPACE.'::validation.enum_meta');
         Validator::extend('enum_meta', function ($attribute, $value, $parameters, $validator) {
             return (new EnumMeta(...$parameters))->passes($attribute, $value);
-        }, trans(static::LANG_NAMESPACE . '::validation.enum_meta'));
+        }, trans(static::LANG_NAMESPACE.'::validation.enum_meta'));
     }
 
     private function bootTranslations(): void
     {
         $this->publishes([
-            __DIR__ . '/../lang' => $this->app->langPath('vendor/' . static::LANG_NAMESPACE),
+            __DIR__.'/../lang' => $this->app->langPath('vendor/'.static::LANG_NAMESPACE),
         ], 'translations');
 
-        $this->loadTranslationsFrom(__DIR__ . '/../lang', static::LANG_NAMESPACE);
+        $this->loadTranslationsFrom(__DIR__.'/../lang', static::LANG_NAMESPACE);
     }
 }
