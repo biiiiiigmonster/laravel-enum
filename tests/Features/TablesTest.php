278829442e996778e9f973a0e4ef5096ebb2df9b
陆云峰 <v-luyunfeng@xiaomi.com>
2023-06-29 20:10:18 +0800

    readme

--- tests/Features/TablesTest.php
+++ tests/Features/TablesTest.php
@@ -2,12 +2,16 @@
 
 use BiiiiiigMonster\LaravelEnum\Tests\Enums\Role;
 use BiiiiiigMonster\LaravelEnum\Tests\Enums\Status;
+use Pest\Expectation;
 
 it('can return an indexed array of tables from a backed enum', function () {
     foreach (Status::tables() as $table) {
         expect($table)
             ->toBeArray()
-            ->toHaveKeys(['name', 'value', 'color', 'description', 'help']);
+            ->when($table['name'] == 'PENDING',
+                fn (Expectation $table) => $table->toHaveKeys(['name', 'value', 'color', 'description']))
+            ->when($table['name'] == 'DONE',
+                fn (Expectation $table) => $table->toHaveKeys(['name', 'value', 'color', 'description', 'help']));
     }
 });
 
