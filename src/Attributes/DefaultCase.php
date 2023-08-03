290c5629b7940afca65709462563f91467a80438
陆云峰 <v-luyunfeng@xiaomi.com>
2023-08-03 10:09:57 +0800

    feat(trait): add static `default` method.

--- src/Attributes/DefaultCase.php
+++ src/Attributes/DefaultCase.php
@@ -4,7 +4,7 @@ namespace BiiiiiigMonster\LaravelEnum\Attributes;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_METHOD)]
+#[Attribute(Attribute::TARGET_CLASS_CONSTANT)]
 class DefaultCase
 {
     //
